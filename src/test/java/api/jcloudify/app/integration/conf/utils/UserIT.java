ce0d76e39753280e4ee293295b81417f4ea4d2d9
Onitsiky <hei.onitsiky@gmail.com>
2024-05-23 14:09:04 +0300

    chore: whoami tests

---
+++ src/test/java/api/jcloudify/app/integration/conf/utils/UserIT.java
@@ -0,0 +1,50 @@
+package api.jcloudify.app.integration.conf.utils;
+
+import api.jcloudify.app.conf.FacadeIT;
+import api.jcloudify.app.endpoint.rest.api.SecurityApi;
+import api.jcloudify.app.endpoint.rest.client.ApiClient;
+import api.jcloudify.app.endpoint.rest.client.ApiException;
+import api.jcloudify.app.endpoint.rest.model.Whoami;
+import api.jcloudify.app.endpoint.rest.security.github.GithubComponent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static api.jcloudify.app.integration.conf.utils.TestMocks.joeDoeUser;
+import static api.jcloudify.app.integration.conf.utils.TestUtils.setUpGithub;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Testcontainers
+@AutoConfigureMockMvc
+public class UserIT extends FacadeIT {
+    @MockBean
+    GithubComponent githubComponent;
+
+    @LocalServerPort private int port;
+
+    private ApiClient anApiClient() {
+        return TestUtils.anApiClient(TestMocks.JOE_DOE_TOKEN, port);
+    }
+
+    static Whoami authenticated() {
+        return new Whoami().user(joeDoeUser());
+    }
+
+    @BeforeEach
+    void setup() {
+        setUpGithub(githubComponent);
+    }
+
+    @Test
+    void whoami_ok() throws ApiException {
+        ApiClient joeDoeClient = anApiClient();
+        SecurityApi api = new SecurityApi(joeDoeClient);
+
+        Whoami actual = api.whoami();
+
+        assertEquals(authenticated(), actual);
+    }
+}
