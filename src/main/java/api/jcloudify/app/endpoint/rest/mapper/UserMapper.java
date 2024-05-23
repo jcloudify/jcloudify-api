6a01b8db971d7883a560688cf96f029642ff7dd2
Onitsiky <hei.onitsiky@gmail.com>
2024-05-23 13:09:53 +0300

    docs(api): return the current user in whoami

---
+++ src/main/java/api/jcloudify/app/endpoint/rest/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package api.jcloudify.app.endpoint.rest.mapper;
+
+import api.jcloudify.app.endpoint.rest.model.Whoami;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public Whoami
+}
