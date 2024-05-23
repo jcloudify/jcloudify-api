2cc9aeaceee2a2afca0bcff2ac37961ee0cf60f5
Onitsiky <hei.onitsiky@gmail.com>
2024-05-23 14:09:04 +0300

    chore: whoami tests

--- src/test/java/api/jcloudify/app/integration/conf/utils/TestMocks.java
+++ src/test/java/api/jcloudify/app/integration/conf/utils/TestMocks.java
@@ -4,6 +4,8 @@ import api.jcloudify.app.endpoint.rest.model.Plan;
 import api.jcloudify.app.endpoint.rest.model.User;
 import java.math.BigDecimal;
 
+import static api.jcloudify.app.endpoint.rest.model.User.RoleEnum.USER;
+
 public class TestMocks {
   public static final String JOE_DOE_ID = "joe_doe_id";
   public static final String JOE_DOE_TOKEN = "joe_doe_token";
@@ -14,6 +16,8 @@ public class TestMocks {
     return new User()
         .id(JOE_DOE_ID)
         .email(JOE_DOE_EMAIL)
+        .username("JoeDoe")
+        .role(USER)
         .firstName("Joe")
         .lastName("Doe")
         .githubId("1234")
@@ -21,6 +25,6 @@ public class TestMocks {
   }
 
   public static Plan freePlan() {
-    return new Plan().id(FREE_PLAN_ID).name("free").cost(BigDecimal.ZERO);
+    return new Plan().id(FREE_PLAN_ID).name("free").cost(0.0);
   }
 }
