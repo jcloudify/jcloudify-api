e672b7a15b7e7b1ad6444db352656b970bb648a4
Onitsiky <hei.onitsiky@gmail.com>
2024-05-23 14:35:32 +0300

    chore: configure cors

---
+++ src/main/java/api/jcloudify/app/endpoint/rest/controller/configurer/CorsConfigurer.java
@@ -0,0 +1,13 @@
+package api.jcloudify.app.endpoint.rest.controller.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedMethods("*").allowedHeaders("*");
+  }
+}
---
+++ src/test/java/api/jcloudify/app/integration/SpringSecurityIT.java
@@ -0,0 +1,76 @@
+package api.jcloudify.app.integration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
+
+import api.jcloudify.app.conf.FacadeIT;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@AutoConfigureMockMvc
+public class SpringSecurityIT extends FacadeIT {
+  @LocalServerPort private int port;
+
+  @Test
+  void ping_with_cors() throws IOException, InterruptedException {
+    HttpClient unauthenticatedClient = HttpClient.newBuilder().build();
+    String basePath = "http://localhost:" + port;
+
+    HttpResponse<String> response =
+        unauthenticatedClient.send(
+            HttpRequest.newBuilder()
+                .uri(URI.create(basePath + "/ping"))
+                // cors
+                .header("Access-Control-Request-Method", "GET")
+                .header("Origin", "http://localhost:3000")
+                .build(),
+            HttpResponse.BodyHandlers.ofString());
+    assertEquals(HttpStatus.OK.value(), response.statusCode());
+    assertEquals("pong", response.body());
+    // cors
+    var headers = response.headers();
+    var origins = headers.allValues("Access-Control-Allow-Origin");
+    assertEquals(1, origins.size());
+    assertEquals("*", origins.get(0));
+  }
+
+  @Test
+  void options_has_cors_headers() throws IOException, InterruptedException {
+    test_cors(GET, "/token");
+  }
+
+  void test_cors(HttpMethod method, String path) throws IOException, InterruptedException {
+    HttpClient unauthenticatedClient = HttpClient.newBuilder().build();
+    String basePath = "http://localhost:" + port;
+
+    HttpResponse<String> response =
+        unauthenticatedClient.send(
+            HttpRequest.newBuilder()
+                .uri(URI.create(basePath + path))
+                .method(OPTIONS.name(), HttpRequest.BodyPublishers.noBody())
+                .header("Access-Control-Request-Headers", "authorization")
+                .header("Access-Control-Request-Method", method.name())
+                .header("Origin", "http://localhost:3000")
+                .build(),
+            HttpResponse.BodyHandlers.ofString());
+
+    var headers = response.headers();
+    var origins = headers.allValues("Access-Control-Allow-Origin");
+    assertEquals(1, origins.size());
+    assertEquals("*", origins.get(0));
+    var headersList = headers.allValues("Access-Control-Allow-Headers");
+    assertEquals(1, headersList.size());
+    assertEquals("authorization", headersList.get(0));
+  }
+}
