ec990cd58e40e1795bd7044e4066e3614cba7714
Onitsiky <hei.onitsiky@gmail.com>
2024-06-07 16:45:48 +0300

    feat: user must provide email adress during signup

--- src/main/java/api/jcloudify/app/repository/model/mapper/UserMapper.java
+++ src/main/java/api/jcloudify/app/repository/model/mapper/UserMapper.java
@@ -3,10 +3,8 @@ package api.jcloudify.app.repository.model.mapper;
 import static api.jcloudify.app.repository.model.enums.UserRole.USER;
 
 import api.jcloudify.app.endpoint.rest.model.CreateUser;
-import api.jcloudify.app.model.exception.InternalServerErrorException;
 import api.jcloudify.app.repository.model.User;
 import api.jcloudify.app.repository.model.enums.UserRole;
-import java.io.IOException;
 import org.kohsuke.github.GHMyself;
 import org.springframework.stereotype.Component;
 
@@ -14,19 +12,15 @@ import org.springframework.stereotype.Component;
 public class UserMapper {
 
   public User toModel(CreateUser toCreate, GHMyself githubUser) {
-    try {
-      UserRole[] roles = {USER};
-      return User.builder()
-          .firstName(toCreate.getFirstName())
-          .lastName(toCreate.getLastName())
-          .githubId(String.valueOf(githubUser.getId()))
-          .email(githubUser.getEmail())
-          .username(githubUser.getLogin())
-          .avatar(githubUser.getAvatarUrl())
-          .roles(roles)
-          .build();
-    } catch (IOException e) {
-      throw new InternalServerErrorException(e);
-    }
+    UserRole[] roles = {USER};
+    return User.builder()
+        .firstName(toCreate.getFirstName())
+        .lastName(toCreate.getLastName())
+        .githubId(String.valueOf(githubUser.getId()))
+        .email(toCreate.getEmail())
+        .username(githubUser.getLogin())
+        .avatar(githubUser.getAvatarUrl())
+        .roles(roles)
+        .build();
   }
 }
