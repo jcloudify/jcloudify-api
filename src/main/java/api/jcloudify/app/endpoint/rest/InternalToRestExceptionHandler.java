32ea2aec1a47b3f5f82dae9df8b26182692bd9f9
Mahefaa <hei.mahefa@gmail.com>
2024-07-18 10:36:26 +0300

    chore: rename rest.model.Exception to rest.model.ExceptionModel because it clashes with java.lang.Exception

--- src/main/java/api/jcloudify/app/endpoint/rest/InternalToRestExceptionHandler.java
+++ src/main/java/api/jcloudify/app/endpoint/rest/InternalToRestExceptionHandler.java
@@ -1,6 +1,6 @@
 package api.jcloudify.app.endpoint.rest;
 
-import api.jcloudify.app.endpoint.rest.model.Exception;
+import api.jcloudify.app.endpoint.rest.model.ExceptionModel;
 import api.jcloudify.app.model.exception.BadRequestException;
 import api.jcloudify.app.model.exception.ForbiddenException;
 import api.jcloudify.app.model.exception.NotFoundException;
@@ -16,19 +16,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 public class InternalToRestExceptionHandler {
   @ExceptionHandler(value = {BadRequestException.class})
-  ResponseEntity<Exception> handleBadRequest(BadRequestException e) {
+  ResponseEntity<ExceptionModel> handleBadRequest(BadRequestException e) {
     log.info("Bad request", e);
     return new ResponseEntity<>(toRest(e, HttpStatus.BAD_REQUEST), HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(value = {NotFoundException.class})
-  ResponseEntity<Exception> handleNotFound(NotFoundException e) {
+  ResponseEntity<ExceptionModel> handleNotFound(NotFoundException e) {
     log.info("Not found", e);
     return new ResponseEntity<>(toRest(e, HttpStatus.NOT_FOUND), HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(value = {java.lang.Exception.class})
-  ResponseEntity<Exception> handleDefault(java.lang.Exception e) {
+  ResponseEntity<ExceptionModel> handleDefault(java.lang.Exception e) {
     log.error("Internal error", e);
     return new ResponseEntity<>(
         toRest(e, HttpStatus.INTERNAL_SERVER_ERROR), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -40,20 +40,20 @@ public class InternalToRestExceptionHandler {
         ForbiddenException.class,
         AuthenticationException.class
       })
-  ResponseEntity<Exception> handleForbidden(java.lang.Exception e) {
+  ResponseEntity<ExceptionModel> handleForbidden(java.lang.Exception e) {
     /* rest.model.Exception.Type.FORBIDDEN designates both authentication and authorization errors.
      * Hence do _not_ HttpsStatus.UNAUTHORIZED because, counter-intuitively,
      * it's just for authentication.
      * https://stackoverflow.com/questions/3297048/403-forbidden-vs-401-unauthorized-http-responses */
     log.info("Forbidden", e);
-    var restException = new Exception();
+    var restException = new ExceptionModel();
     restException.setType(HttpStatus.FORBIDDEN.toString());
     restException.setMessage(e.getMessage());
     return new ResponseEntity<>(restException, HttpStatus.FORBIDDEN);
   }
 
-  private Exception toRest(java.lang.Exception e, HttpStatus notFound) {
-    var restException = new Exception();
+  private ExceptionModel toRest(java.lang.Exception e, HttpStatus notFound) {
+    var restException = new ExceptionModel();
     restException.setType(HttpStatus.BAD_REQUEST.toString());
     restException.setMessage(e.getMessage());
     return restException;
