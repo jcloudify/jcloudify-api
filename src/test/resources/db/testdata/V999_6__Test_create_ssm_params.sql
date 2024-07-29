bc9759197bbf4d72676d71f44d59d8f2ec70cf2c
Onitsiky <hei.onitsiky@gmail.com>
2024-07-29 09:27:31 +0300

    chore(tests): get and crupdate ssm parameters

---
+++ src/test/resources/db/testdata/V999_6__Test_create_ssm_params.sql
@@ -0,0 +1,3 @@
+insert into "ssm_parameter"(id, param_name, id_environment)
+values ('ssm_param_1_id', '/poja/prod/ssm/param1', 'poja_application_environment_id'),
+       ('ssm_param_2_id', '/poja/prod/ssm/param2', 'poja_application_environment_id');
