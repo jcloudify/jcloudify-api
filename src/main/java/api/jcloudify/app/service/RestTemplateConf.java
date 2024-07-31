24106d146c6f9be911561702c4245839c21acf3b
Mahefaa <hei.mahefa@gmail.com>
2024-07-31 10:41:07 +0300

    chore: RestTemplateErrorHandler

--- src/main/java/api/jcloudify/app/service/RestTemplateConf.java
+++ src/main/java/api/jcloudify/app/service/RestTemplateConf.java
@@ -1,9 +1,15 @@
 package api.jcloudify.app.service;
 
+import static api.jcloudify.app.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
+
+import api.jcloudify.app.model.exception.ApiException;
+import java.io.IOException;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -12,7 +18,29 @@ public class RestTemplateConf {
   RestTemplate restTemplate(RestTemplateBuilder builder) {
     HttpComponentsClientHttpRequestFactory requestFactory =
         new HttpComponentsClientHttpRequestFactory();
-    builder.requestFactory(() -> requestFactory);
-    return builder.build();
+    return builder
+        .requestFactory(() -> requestFactory)
+        .errorHandler(new RestTemplateErrorHandler())
+        .build();
+  }
+
+  private static class RestTemplateErrorHandler implements ResponseErrorHandler {
+    @Override
+    public boolean hasError(ClientHttpResponse response) throws IOException {
+      return response.getStatusCode().is4xxClientError()
+          || response.getStatusCode().is5xxServerError();
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+      var statusCode = response.getStatusCode();
+      if (statusCode.is4xxClientError()) {
+        throw new ApiException(
+            SERVER_EXCEPTION, "client exception with status " + response.getStatusCode());
+      } else if (statusCode.is5xxServerError()) {
+        throw new ApiException(
+            SERVER_EXCEPTION, "server exception with status " + response.getStatusCode());
+      }
+    }
   }
 }
