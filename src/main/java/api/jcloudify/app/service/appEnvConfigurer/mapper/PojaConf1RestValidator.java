15924f09803b654a07f26525c926b5de6ffde8d4
tanjonaaa <hei.tanjona@gmail.com>
2024-08-08 13:37:50 +0300

    test: validator_validate_PojaConf1_ok && validator_validate_PojaConf1_ko

--- src/main/java/api/jcloudify/app/service/appEnvConfigurer/mapper/PojaConf1RestValidator.java
+++ src/main/java/api/jcloudify/app/service/appEnvConfigurer/mapper/PojaConf1RestValidator.java
@@ -1,8 +1,14 @@
 package api.jcloudify.app.service.appEnvConfigurer.mapper;
 
+import api.jcloudify.app.endpoint.rest.model.ComputeConf1;
+import api.jcloudify.app.endpoint.rest.model.ConcurrencyConf1;
+import api.jcloudify.app.endpoint.rest.model.DatabaseConf1;
+import api.jcloudify.app.endpoint.rest.model.GeneralPojaConf1;
+import api.jcloudify.app.endpoint.rest.model.Integration1;
+import api.jcloudify.app.endpoint.rest.model.MailingConf1;
 import api.jcloudify.app.endpoint.rest.model.PojaConf1;
+import api.jcloudify.app.endpoint.rest.model.TestingConf1;
 import api.jcloudify.app.model.exception.BadRequestException;
-import java.util.List;
 import java.util.function.Consumer;
 import java.util.regex.Pattern;
 import org.springframework.stereotype.Component;
@@ -14,107 +20,117 @@ public class PojaConf1RestValidator implements Consumer<PojaConf1> {
   public void accept(PojaConf1 pojaConf1) {
     StringBuilder exceptionMessageBuilder = new StringBuilder();
 
+    GeneralPojaConf1 generalConf = pojaConf1.getGeneral();
+    Integration1 integrationConf = pojaConf1.getIntegration();
+    MailingConf1 mailingConf = pojaConf1.getEmailing();
+    TestingConf1 testingConf = pojaConf1.getTesting();
+    ComputeConf1 computeConf = pojaConf1.getCompute();
+    DatabaseConf1 databaseConf = pojaConf1.getDatabase();
+    ConcurrencyConf1 concurrencyConf = pojaConf1.getConcurrency();
+
     if (pojaConf1.getVersion() == null) {
       exceptionMessageBuilder.append("version is mandatory. ");
     }
-    if (pojaConf1.getGeneral() == null) {
+    if (generalConf == null) {
       exceptionMessageBuilder.append("general is mandatory. ");
     } else {
-      if (pojaConf1.getGeneral().getAppName() == null) {
+      if (generalConf.getAppName() == null) {
         exceptionMessageBuilder.append("app_name is mandatory. ");
       }
-      if (pojaConf1.getGeneral().getWithSnapstart() == null) {
+      if (generalConf.getWithSnapstart() == null) {
         exceptionMessageBuilder.append("with_snapstart is mandatory. ");
       }
-      if (pojaConf1.getGeneral().getPackageFullName() == null) {
+      if (generalConf.getPackageFullName() == null) {
         exceptionMessageBuilder.append("package_full_name is mandatory. ");
+      } else {
+        if (!isPackageFullNameValid(generalConf.getPackageFullName())) {
+          exceptionMessageBuilder.append(
+              "package_full_name must include three parts separated by dots. ");
+        }
       }
-      if (pojaConf1.getGeneral().getWithQueuesNb() == null) {
+      if (generalConf.getWithQueuesNb() == null) {
         exceptionMessageBuilder.append("queues_nb is mandatory. ");
       }
-      if (pojaConf1.getGeneral().getCustomJavaDeps() == null) {
+      if (generalConf.getCustomJavaDeps() == null) {
         exceptionMessageBuilder.append("custom_java_deps is mandatory. ");
       }
-      if (pojaConf1.getGeneral().getCustomJavaEnvVars() == null) {
+      if (generalConf.getCustomJavaEnvVars() == null) {
         exceptionMessageBuilder.append("custom_java_env_vars is mandatory. ");
       }
-      if (pojaConf1.getGeneral().getCustomJavaRepositories() == null) {
+      if (generalConf.getCustomJavaRepositories() == null) {
         exceptionMessageBuilder.append("custom_java_repositories is mandatory. ");
       }
-      if (!isPackageFullNameValid(pojaConf1.getGeneral().getPackageFullName())) {
-        exceptionMessageBuilder.append(
-            "package_full_name must include three parts separated by dots. ");
-      }
     }
-    if (pojaConf1.getIntegration() == null) {
+    if (integrationConf == null) {
       exceptionMessageBuilder.append("integration is mandatory. ");
     } else {
-      if (pojaConf1.getIntegration().getWithSwaggerUi() == null) {
+      if (integrationConf.getWithSwaggerUi() == null) {
         exceptionMessageBuilder.append("with_swagger_ui is mandatory. ");
       }
-      if (pojaConf1.getIntegration().getWithCodeql() == null) {
+      if (integrationConf.getWithCodeql() == null) {
         exceptionMessageBuilder.append("with_codeql is mandatory. ");
       }
-      if (pojaConf1.getIntegration().getWithFileStorage() == null) {
+      if (integrationConf.getWithFileStorage() == null) {
         exceptionMessageBuilder.append("with_file_storage is mandatory. ");
       }
-      if (pojaConf1.getIntegration().getWithSentry() == null) {
+      if (integrationConf.getWithSentry() == null) {
         exceptionMessageBuilder.append("with_sentry is mandatory. ");
       }
-      if (pojaConf1.getIntegration().getWithSonar() == null) {
+      if (integrationConf.getWithSonar() == null) {
         exceptionMessageBuilder.append("with_sonar is mandatory. ");
       }
     }
-    if (pojaConf1.getEmailing() == null) {
+    if (mailingConf == null) {
       exceptionMessageBuilder.append("emailing is mandatory. ");
     } else {
-      if (pojaConf1.getEmailing().getSesSource() == null) {
+      if (mailingConf.getSesSource() == null) {
         exceptionMessageBuilder.append("ses_source is mandatory. ");
-      }
-      if (!isAValidEmail(pojaConf1.getEmailing().getSesSource())) {
-        exceptionMessageBuilder.append("ses_source must be a valid email address. ");
+      } else {
+        if (!isAValidEmail(mailingConf.getSesSource())) {
+          exceptionMessageBuilder.append("ses_source must be a valid email address. ");
+        }
       }
     }
-    if (pojaConf1.getTesting() == null) {
+    if (testingConf == null) {
       exceptionMessageBuilder.append("testing is mandatory. ");
     } else {
-      if (pojaConf1.getTesting().getJacocoMinCoverage() == null) {
+      if (testingConf.getJacocoMinCoverage() == null) {
         exceptionMessageBuilder.append("jacoco_min_coverage is mandatory. ");
       }
-      if (pojaConf1.getTesting().getJavaFacadeIt() == null) {
+      if (testingConf.getJavaFacadeIt() == null) {
         exceptionMessageBuilder.append("java_facade_it is mandatory. ");
       }
     }
-    if (pojaConf1.getCompute() == null) {
+    if (computeConf == null) {
       exceptionMessageBuilder.append("compute is mandatory. ");
     } else {
-      if (pojaConf1.getCompute().getFrontalMemory() == null) {
+      if (computeConf.getFrontalMemory() == null) {
         exceptionMessageBuilder.append("compute_frontal_memory is mandatory. ");
       }
-      if (pojaConf1.getCompute().getFrontalFunctionTimeout() == null) {
+      if (computeConf.getFrontalFunctionTimeout() == null) {
         exceptionMessageBuilder.append("frontal_function_timeout is mandatory. ");
       }
-      if (pojaConf1.getCompute().getWorkerMemory() == null) {
+      if (computeConf.getWorkerMemory() == null) {
         exceptionMessageBuilder.append("worker_memory is mandatory. ");
       }
-      if (pojaConf1.getCompute().getWorkerFunction1Timeout() == null) {
+      if (computeConf.getWorkerFunction1Timeout() == null) {
         exceptionMessageBuilder.append("worker_function_1_timeout is mandatory. ");
       }
-      if (pojaConf1.getCompute().getWorkerFunction2Timeout() == null) {
+      if (computeConf.getWorkerFunction2Timeout() == null) {
         exceptionMessageBuilder.append("worker_function_2_timeout is mandatory. ");
       }
-      if (pojaConf1.getCompute().getWorkerBatch() == null) {
+      if (computeConf.getWorkerBatch() == null) {
         exceptionMessageBuilder.append("worker_batch is mandatory. ");
       }
     }
-    if (pojaConf1.getDatabase() == null) {
+    if (databaseConf == null) {
       exceptionMessageBuilder.append("database is mandatory. ");
     } else {
-      if (pojaConf1.getDatabase().getWithDatabase() == null) {
+      if (databaseConf.getWithDatabase() == null) {
         exceptionMessageBuilder.append("with_database is mandatory. ");
       }
     }
-    if (pojaConf1.getConcurrency() == null) {
+    if (concurrencyConf == null) {
       exceptionMessageBuilder.append("concurrency is mandatory. ");
     }
     String exceptionMessage = exceptionMessageBuilder.toString();
@@ -124,7 +140,7 @@ public class PojaConf1RestValidator implements Consumer<PojaConf1> {
   }
 
   private boolean isPackageFullNameValid(String packageFullName) {
-    return List.of(packageFullName.split("\\.")).size() == 3;
+    return packageFullName.split("\\.").length == 3;
   }
 
   private boolean isAValidEmail(String email) {
