8691aacff36f4bd5240bda96f1ac9d6876f2d000
Onitsiky <hei.onitsiky@gmail.com>
2024-08-19 13:44:54 +0300

    chore: refactor stack deployment methods

--- src/main/java/api/jcloudify/app/aws/cloudformation/CloudformationTemplateConf.java
+++ src/main/java/api/jcloudify/app/aws/cloudformation/CloudformationTemplateConf.java
@@ -1,7 +1,10 @@
 package api.jcloudify.app.aws.cloudformation;
 
-import api.jcloudify.app.file.BucketComponent;
-import java.net.URL;
+import static api.jcloudify.app.file.ExtendedBucketComponent.getBucketKey;
+import static api.jcloudify.app.file.FileType.DEPLOYMENT_FILE;
+
+import api.jcloudify.app.file.ExtendedBucketComponent;
+import java.net.URI;
 import java.time.Duration;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,26 +12,13 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 @Component
 public class CloudformationTemplateConf {
-  private final BucketComponent bucketComponent;
-  private static final String CF_STACK_TEMPLATE_FOLDER = "cf-templates/";
+  private final ExtendedBucketComponent extendedBucketComponent;
+  private final Duration TEMPLATE_PRESIGNED_URL_DURATION = Duration.ofMinutes(10);
 
-  public URL getEventStackTemplateUrl() {
-    String eventStackTemplatePath = CF_STACK_TEMPLATE_FOLDER + "event-stack.yml";
-    return bucketComponent.presign(eventStackTemplatePath, Duration.ofMinutes(5));
-  }
-
-  public URL getComputePermissionStackTemplateUrl() {
-    String eventStackTemplatePath = CF_STACK_TEMPLATE_FOLDER + "compute-permission-stack.yml";
-    return bucketComponent.presign(eventStackTemplatePath, Duration.ofMinutes(1));
-  }
-
-  public URL getStorageBucketStackTemplateUrl() {
-    String eventStackTemplatePath = CF_STACK_TEMPLATE_FOLDER + "storage-bucket-stack.yml";
-    return bucketComponent.presign(eventStackTemplatePath, Duration.ofMinutes(3));
-  }
-
-  public URL getStorageDatabaseSQliteStackTemplateUrl() {
-    String eventStackTemplatePath = CF_STACK_TEMPLATE_FOLDER + "storage-efs-stack.yml";
-    return bucketComponent.presign(eventStackTemplatePath, Duration.ofMinutes(5));
+  public URI getCloudformationTemplateUrl(
+      String userId, String appId, String envId, String filename) {
+    String formattedBucketKey = getBucketKey(userId, appId, envId, DEPLOYMENT_FILE) + filename;
+    return extendedBucketComponent.presignGetObject(
+        formattedBucketKey, TEMPLATE_PRESIGNED_URL_DURATION);
   }
 }
--- src/main/java/api/jcloudify/app/repository/jpa/EnvDeploymentConfRepository.java
+++ src/main/java/api/jcloudify/app/repository/jpa/EnvDeploymentConfRepository.java
@@ -1,6 +1,9 @@
 package api.jcloudify.app.repository.jpa;
 
 import api.jcloudify.app.repository.model.EnvDeploymentConf;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EnvDeploymentConfRepository extends JpaRepository<EnvDeploymentConf, String> {}
+public interface EnvDeploymentConfRepository extends JpaRepository<EnvDeploymentConf, String> {
+  Optional<EnvDeploymentConf> findTopByEnvIdOrderByCreationDatetimeDesc(String envId);
+}
