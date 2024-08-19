2d668c21f57e4cd99d323fddf8ff79bab5f00348
Onitsiky <hei.onitsiky@gmail.com>
2024-08-19 11:55:50 +0300

    chore: only one stack type per env is authorized

--- src/main/java/api/jcloudify/app/repository/model/Stack.java
+++ src/main/java/api/jcloudify/app/repository/model/Stack.java
@@ -1,12 +1,15 @@
 package api.jcloudify.app.repository.model;
 
 import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
 import static org.hibernate.type.SqlTypes.NAMED_ENUM;
 
 import api.jcloudify.app.endpoint.rest.model.StackType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.io.Serializable;
@@ -32,7 +35,9 @@ import org.hibernate.annotations.UpdateTimestamp;
 @EqualsAndHashCode
 @ToString
 public class Stack implements Serializable {
-  @Id private String id;
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private String id;
 
   private String name;
 
