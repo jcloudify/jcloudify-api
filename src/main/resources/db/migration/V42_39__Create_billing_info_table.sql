a45d132f3fa338bfad3a12cb1575f93d92de05f2
Mahefaa <hei.mahefa@gmail.com>
2024-09-04 16:23:29 +0300

    chore: billing info

---
+++ src/main/resources/db/migration/V42_39__Create_billing_info_table.sql
@@ -0,0 +1,27 @@
+do
+$$
+    begin
+        if not exists(select from pg_type where typname = 'billing_info_compute_status') then
+            create type billing_info_compute_status as enum ('PENDING','IN_PROGRESS','FINISHED');
+        end if;
+    end
+$$;
+
+create table if not exists "billing_info"
+(
+    id                           varchar primary key         default uuid_generate_v4(),
+    creation_datetime            timestamp without time zone,
+    compute_datetime             timestamp without time zone,
+    user_id                      varchar not null,
+    app_id                       varchar not null,
+    env_id                       varchar not null,
+    query_id                     varchar not null,
+    pricing_method               varchar not null,
+    computed_price_in_usd        numeric not null,
+    computed_duration_in_minutes numeric not null,
+    computed_memory_used_in_mo   numeric not null,
+    status                       billing_info_compute_status default 'PENDING',
+    constraint fk_user_billing foreign key (user_id) references "user" (id),
+    constraint fk_app_billing foreign key (app_id) references "application" (id),
+    constraint fk_env_billing foreign key (env_id) references "environment" (id)
+);
