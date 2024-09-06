de948ea0cfc85c8932f4e13629c253eb91ef5212
Mahefaa <hei.mahefa@gmail.com>
2024-09-06 11:03:27 +0300

    chore: get billing info result and extract duration and memoryUsage

---
+++ src/main/java/api/jcloudify/app/endpoint/event/model/GetBillingInfoQueryResultRequested.java
@@ -0,0 +1,25 @@
+package api.jcloudify.app.endpoint.event.model;
+
+import java.time.Duration;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
+@AllArgsConstructor
+public class GetBillingInfoQueryResultRequested extends PojaEvent {
+  private final String queryId;
+
+  @Override
+  public Duration maxConsumerDuration() {
+    return Duration.ofSeconds(30);
+  }
+
+  @Override
+  public Duration maxConsumerBackoffBetweenRetries() {
+    return Duration.ofMinutes(15);
+  }
+}
