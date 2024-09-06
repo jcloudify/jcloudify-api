f83dc64866069e53d604ac4e8781aeedd556f10e
Mahefaa <hei.mahefa@gmail.com>
2024-09-06 10:32:46 +0300

    chore: send log insights query for sum(duration) and sum(memoryUsed)

--- src/main/java/api/jcloudify/app/service/event/RefreshEnvBillingInfoRequestedService.java
+++ src/main/java/api/jcloudify/app/service/event/RefreshEnvBillingInfoRequestedService.java
@@ -2,28 +2,94 @@ package api.jcloudify.app.service.event;
 
 import api.jcloudify.app.aws.cloudwatch.CloudwatchComponent;
 import api.jcloudify.app.endpoint.event.model.RefreshEnvBillingInfoRequested;
-import api.jcloudify.app.service.LambdaFunctionLogService;
+import api.jcloudify.app.repository.model.Application;
+import api.jcloudify.app.repository.model.Environment;
+import api.jcloudify.app.service.ApplicationService;
+import api.jcloudify.app.service.EnvironmentService;
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.cloudwatchlogs.model.LogGroup;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class RefreshEnvBillingInfoRequestedService
     implements Consumer<RefreshEnvBillingInfoRequested> {
+  private static final String LOG_INSIGHTS_QUERY_SUM_DURATION_AND_MEMORY =
+      """
+    fields @timestamp, @maxMemoryUsed, @duration
+    | filter @message like /REPORT RequestId:/
+    | stats\s
+        sum(@duration)/ 60000 as totalDurationMinutes,
+        sum(@maxMemoryUsed)/ 1048576 as totalMemoryMB
+    """;
+  private static final String LOG_GROUP_NAME_PATTERN_FOR_FRONTAL_FUNCTION =
+      "%s-compute-%s-FrontalFunction";
+  private static final String LOG_GROUP_NAME_PATTERN_FOR_WORKER_FUNCTIONS =
+      "%s-compute-%s-WorkerFunction";
   private final CloudwatchComponent cloudwatchComponent;
-  private final LambdaFunctionLogService lambdaFunctionLogService;
+  private final ApplicationService applicationService;
+  private final EnvironmentService environmentService;
 
   @Override
   public void accept(RefreshEnvBillingInfoRequested rebirEvent) {
-    var computeStackResource =
-    var logGroups =
-        lambdaFunctionLogService.getLogGroups(
-            rebirEvent.getUserId(), rebirEvent.getAppId(), rebirEvent.getEnvId());
-    cloudwatchComponent.initiateLogInsightsQuery(
-        """
-""",
-        null, null, List.of());
+    var app = applicationService.getById(rebirEvent.getAppId());
+    var env = environmentService.getById(rebirEvent.getEnvId());
+    Instant startTime = rebirEvent.getPricingCalculationRequestStartTime();
+    Instant endTime = rebirEvent.getPricingCalculationRequestEndTime();
+    var logGroups = getLogGroupNamesBetweenDatesRangeInclusive(app, env, startTime, endTime);
+    var queryId =
+        cloudwatchComponent.initiateLogInsightsQuery(
+            LOG_INSIGHTS_QUERY_SUM_DURATION_AND_MEMORY, startTime, endTime, logGroups);
+    log.info("query {} initiated", queryId);
+    // save queryId and send getResult event
+  }
+
+  private static String formatEnvTypeToLogGroupPattern(Environment env) {
+    return env.getEnvironmentType().name().toLowerCase();
+  }
+
+  private static String formatFrontalFunctionLogGroupNamePattern(Application app, Environment env) {
+    return LOG_GROUP_NAME_PATTERN_FOR_FRONTAL_FUNCTION.formatted(
+        formatEnvTypeToLogGroupPattern(env), app.getName());
+  }
+
+  private static String formatWorkerFunctionLogGroupNamePattern(Application app, Environment env) {
+    return LOG_GROUP_NAME_PATTERN_FOR_WORKER_FUNCTIONS.formatted(
+        formatEnvTypeToLogGroupPattern(env), app.getName());
+  }
+
+  private List<String> getLogGroupNamesBetweenDatesRangeInclusive(
+      Application app, Environment env, Instant startTime, Instant endTime) {
+    List<LogGroup> rawFrontalLogGroups =
+        cloudwatchComponent.getLambdaFunctionLogGroupsByNamePattern(
+            formatFrontalFunctionLogGroupNamePattern(app, env));
+    List<LogGroup> rawWorkerLogGroups =
+        cloudwatchComponent.getLambdaFunctionLogGroupsByNamePattern(
+            formatWorkerFunctionLogGroupNamePattern(app, env));
+    return getLogGroupNamesFilteredByCreationTimeBetween(
+        rawFrontalLogGroups, rawWorkerLogGroups, startTime, endTime);
+  }
+
+  private List<String> getLogGroupNamesFilteredByCreationTimeBetween(
+      List<LogGroup> frontalLogGroups,
+      List<LogGroup> workerLogGroups,
+      Instant startTime,
+      Instant endTime) {
+    var logGroups = new ArrayList<>(frontalLogGroups);
+    logGroups.addAll(workerLogGroups);
+    return logGroups.stream()
+        .filter(
+            lg -> {
+              var ct = Instant.ofEpochMilli(lg.creationTime());
+              return ct.compareTo(startTime) >= 0 && ct.compareTo(endTime) <= 0;
+            })
+        .map(LogGroup::logGroupName)
+        .toList();
   }
 }
