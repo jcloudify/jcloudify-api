077096665f9559b73df34e0425f5ded2f5015b73
Onitsiky Ranaivoson <hei.onitsiky@gmail.com>
2024-09-12 16:05:17 +0300

    docs(api): get list of deployment progression

--- src/main/java/api/jcloudify/app/model/StateMachine.java
+++ src/main/java/api/jcloudify/app/model/StateMachine.java
@@ -1,20 +1,20 @@
 package api.jcloudify.app.model;
 
-import lombok.AllArgsConstructor;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public abstract class StateMachine<STATE_ENUM extends Enum<STATE_ENUM>> {
-    private final List<State<STATE_ENUM>> states;
+  private final List<State<STATE_ENUM>> states;
 
-    public abstract List<State<STATE_ENUM>> addState(State<STATE_ENUM> state);
+  public abstract List<State<STATE_ENUM>> addState(State<STATE_ENUM> state);
 
-    public State<STATE_ENUM> getLatestState() {
-        var list = new ArrayList<>(states);
-        list.sort(Comparator.comparing(State::getT));
-        return list.getFirst();
-    };
+  public State<STATE_ENUM> getLatestState() {
+    var list = new ArrayList<>(states);
+    list.sort(Comparator.comparing(State::getT));
+    return list.getFirst();
+  }
+  ;
 }
