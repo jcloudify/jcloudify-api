29f90df2aa024c4177e81fd90f0ee0025965ea89
tanjonaaa <hei.tanjona@gmail.com>
2024-09-17 13:51:45 +0300

    feat: save computed billed duration and computed price in billing info

--- src/main/java/api/jcloudify/app/service/event/GetBillingInfoQueryResultRequestedService.java
+++ src/main/java/api/jcloudify/app/service/event/GetBillingInfoQueryResultRequestedService.java
@@ -1,12 +1,14 @@
 package api.jcloudify.app.service.event;
 
-import static api.jcloudify.app.service.pricing.PricingMethod.TEN_MICRO;
+import static api.jcloudify.app.repository.model.enums.BillingInfoComputeStatus.FINISHED;
+import static java.time.Instant.now;
 import static software.amazon.awssdk.services.cloudwatchlogs.model.QueryStatus.COMPLETE;
 import static software.amazon.awssdk.services.cloudwatchlogs.model.QueryStatus.FAILED;
 import static software.amazon.awssdk.services.cloudwatchlogs.model.QueryStatus.RUNNING;
 
 import api.jcloudify.app.aws.cloudwatch.CloudwatchComponent;
 import api.jcloudify.app.endpoint.event.model.GetBillingInfoQueryResultRequested;
+import api.jcloudify.app.service.BillingInfoService;
 import api.jcloudify.app.service.pricing.calculator.PricingCalculator;
 import java.math.BigDecimal;
 import java.util.List;
@@ -25,14 +27,15 @@ public class GetBillingInfoQueryResultRequestedService
     implements Consumer<GetBillingInfoQueryResultRequested> {
   private final CloudwatchComponent cloudwatchComponent;
   private final PricingCalculator pricingCalculator;
+  private final BillingInfoService billingInfoService;
 
   @Override
   public void accept(GetBillingInfoQueryResultRequested event) {
     GetQueryResultsResponse getQueryResultsResponse =
         cloudwatchComponent.getQueryResult(event.getQueryId());
     QueryStatus status = getQueryResultsResponse.status();
-    // TODO: get billing info with queryId = event.getQueryId and take its pricingMethod
-    var pricingMethod = TEN_MICRO;
+    var billingInfo = billingInfoService.getByQueryId(event.getQueryId());
+    var pricingMethod = billingInfo.getPricingMethod();
 
     if (RUNNING.equals(status)) {
       // fail on purpose so event backs off
@@ -44,11 +47,17 @@ public class GetBillingInfoQueryResultRequestedService
       List<ResultField> first = results.getFirst();
       assert first.size() == 2;
       var billedMemoryDuration = first.getFirst();
+      var computedBilledDuration = first.get(1);
       assert "billedMemoryDuration".equals(billedMemoryDuration.field());
+      assert "computedBilledDuration".equals(computedBilledDuration.field());
       var computedPrice =
           pricingCalculator.computePrice(
               pricingMethod, new BigDecimal(billedMemoryDuration.value()));
-      // TODO: update billing info with queryId = event.getQueryId
+      billingInfo.setComputeDatetime(now());
+      billingInfo.setComputedPriceInUsd(computedPrice);
+      billingInfo.setComputedDurationInMinutes(Integer.valueOf(computedBilledDuration.value()));
+      billingInfo.setStatus(FINISHED);
+      billingInfoService.crupdateBillingInfo(billingInfo);
     } else if (FAILED.equals(status)) {
       log.info("query with ID {} failed, please inspect cloudwatch", event.getQueryId());
       // what do we do on query fail?
--- src/main/java/api/jcloudify/app/service/event/RefreshEnvBillingInfoRequestedService.java
+++ src/main/java/api/jcloudify/app/service/event/RefreshEnvBillingInfoRequestedService.java
@@ -5,9 +5,12 @@ import api.jcloudify.app.endpoint.event.EventProducer;
 import api.jcloudify.app.endpoint.event.model.GetBillingInfoQueryResultRequested;
 import api.jcloudify.app.endpoint.event.model.RefreshEnvBillingInfoRequested;
 import api.jcloudify.app.repository.model.Application;
+import api.jcloudify.app.repository.model.BillingInfo;
 import api.jcloudify.app.repository.model.Environment;
 import api.jcloudify.app.service.ApplicationService;
+import api.jcloudify.app.service.BillingInfoService;
 import api.jcloudify.app.service.EnvironmentService;
+import api.jcloudify.app.service.UserService;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,10 +28,11 @@ public class RefreshEnvBillingInfoRequestedService
     implements Consumer<RefreshEnvBillingInfoRequested> {
   private static final String LOG_INSIGHTS_QUERY_TOTAL_MEMORY_DURATION =
       """
+
 fields @timestamp, @billedDuration/60000 as durationInMinutes, @memorySize/(1000000) as memorySizeInMo
  | filter @message like /REPORT RequestId:/
- | stats sum(durationInMinutes * memorySizeInMo) as billedMemoryDurationGrouped by memorySizeInMo
- | stats sum(billedMemoryDurationGrouped) as billedMemoryDuration
+ | stats sum(durationInMinutes * memorySizeInMo) as billedMemoryDurationGrouped, sum(durationInMinutes) as billedDurationGrouped by memorySizeInMo
+ | stats sum(billedMemoryDurationGrouped) as billedMemoryDuration, sum(billedDurationGrouped) as computedBilledDuration
 """;
   private static final String LOG_GROUP_NAME_PATTERN_FOR_FRONTAL_FUNCTION =
       "%s-compute-%s-FrontalFunction";
@@ -37,6 +41,8 @@ fields @timestamp, @billedDuration/60000 as durationInMinutes, @memorySize/(1000
   private final CloudwatchComponent cloudwatchComponent;
   private final ApplicationService applicationService;
   private final EnvironmentService environmentService;
+  private final BillingInfoService billingInfoService;
+  private final UserService userService;
   private final EventProducer<GetBillingInfoQueryResultRequested> eventProducer;
 
   @Override
@@ -50,7 +56,16 @@ fields @timestamp, @billedDuration/60000 as durationInMinutes, @memorySize/(1000
         cloudwatchComponent.initiateLogInsightsQuery(
             LOG_INSIGHTS_QUERY_TOTAL_MEMORY_DURATION, startTime, endTime, logGroups);
     log.info("query {} initiated", queryId);
-    // TODO: save queryId and userId, envId, appId
+    var user = userService.getUserById(app.getUserId());
+    var billingInfoToSave =
+        BillingInfo.builder()
+            .queryId(queryId)
+            .userId(user.getId())
+            .appId(app.getId())
+            .envId(env.getId())
+            .pricingMethod(user.getPricingMethod())
+            .build();
+    billingInfoService.crupdateBillingInfo(billingInfoToSave);
     eventProducer.accept(List.of(new GetBillingInfoQueryResultRequested(queryId)));
   }
 
