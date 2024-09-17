c04c6dc76332ec4cd5a99702c4959655d5ecf7d0
Onitsiky Ranaivoson <hei.onitsiky@gmail.com>
2024-09-17 09:22:53 +0300

    poja-upgrade: 18.1.1

--- .shell/checkAsyncStack.sh
+++ .shell/checkAsyncStack.sh
@@ -2,7 +2,7 @@ sudo apt-get install jq
 export API_URL_SSM="`aws ssm get-parameter --name /jcloudify-api/$1/api/url`"
 export API_URL=`echo $API_URL_SSM | jq -r '.Parameter.Value'`
 created_uuids=$(curl --fail -X GET "$API_URL$2")
-sleep 40
+sleep 90
 output=$(curl -s -X POST -H "Content-Type: application/json" -d "$created_uuids" "$API_URL/health/event/uuids")
 if [ "$output" = "OK" ]; then
   exit 0
