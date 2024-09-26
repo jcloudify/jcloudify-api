4df9f385cf04691ea01e9df4d643a1a7000e3c9f
Onitsiky Ranaivoson <hei.onitsiky@gmail.com>
2024-09-26 16:34:08 +0300

    chore: refactor deployment state

--- src/main/java/api/jcloudify/app/service/workflows/DeploymentStateService.java
+++ src/main/java/api/jcloudify/app/service/workflows/DeploymentStateService.java
@@ -4,10 +4,10 @@ import static api.jcloudify.app.endpoint.rest.model.ExecutionType.ASYNCHRONOUS;
 
 import api.jcloudify.app.endpoint.rest.model.DeploymentStateEnum;
 import api.jcloudify.app.model.BoundedPageSize;
-import api.jcloudify.app.model.Page;
 import api.jcloudify.app.model.PageFromOne;
 import api.jcloudify.app.repository.jpa.DeploymentStateRepository;
 import api.jcloudify.app.repository.model.DeploymentState;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,15 +18,14 @@ import org.springframework.stereotype.Service;
 public class DeploymentStateService {
   private final DeploymentStateRepository repository;
 
-  public Page<DeploymentState> getDeploymentStatesByDeploymentId(
+  public List<DeploymentState> getDeploymentStatesByDeploymentId(
       String userId,
       String appId,
       String deploymentId,
       PageFromOne pageFromOne,
       BoundedPageSize boundedPageSize) {
     Pageable pageable = PageRequest.of(pageFromOne.getValue() - 1, boundedPageSize.getValue());
-    var data = repository.findAllByAppEnvDeploymentId(deploymentId, pageable);
-    return new Page<>(pageFromOne, boundedPageSize, data);
+    return repository.findAllByAppEnvDeploymentId(deploymentId, pageable);
   }
 
   public void save(String appEnvDeploymentId, DeploymentStateEnum status) {
