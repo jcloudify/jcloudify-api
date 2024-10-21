0c2d71be3a31acdebaa4a48751de859b85087dca
Mahefaa <hei.mahefa@gmail.com>
2024-10-21 19:58:42 +0300

    chore: custom poja handler with abstract and facade

--- src/main/java/api/jcloudify/app/service/pojaConfHandler/PojaConfHandler.java
+++
@@ -1,8 +0,0 @@
-package api.jcloudify.app.service.pojaConfHandler;
-
-import api.jcloudify.app.endpoint.rest.model.PojaConf;
-import api.jcloudify.app.model.PojaVersion;
-
-public interface PojaConfHandler {
-  void handlePojaConf(PojaVersion pojaVersion, PojaConf pojaConf);
-}
